270417c2d619de229e422dd192f193167b866a44
zanguedimitri <130470482+Zanguedimitri@users.noreply.github.com>
2024-10-13 17:16:34 +0100

    add banner and order pages (#3)
    create header, footer and contain page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { CoporateComponent } from './pages/coporate/coporate.component';
+import { PersonalBankingServiceComponent } from './pages/personal-banking-service/personal-banking-service.component';
+import { PremiumBscaBankOneComponent } from './pages/premium-bsca-bank-one/premium-bsca-bank-one.component';
+import { PremiumBscaBankTwoComponent } from './pages/premium-bsca-bank-two/premium-bsca-bank-two.component';
+export const routes: Routes = [
+  {path:'', redirectTo:'prenuim', pathMatch:'full' },
+  {path:'coporate', component:CoporateComponent },
+  {path:'prenuim', component:PremiumBscaBankOneComponent},
+  {path:'secondprenuim', component:PremiumBscaBankTwoComponent},
+  {path:'personal', component:PersonalBankingServiceComponent},
+];
