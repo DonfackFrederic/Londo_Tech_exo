ec4e2ab16d8b2038290c2a2764999f9bdeae6a48
Dimitri Zangue <dimitrizangue1@gmail.com>
2024-10-13 16:55:43 +0100

    add banner and order pages

--- src/app/includes/header/header.component.ts
+++ src/app/includes/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+ 
 
 }
--- src/app/pages/coporate/coporate.component.ts
+++ src/app/pages/coporate/coporate.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../includes/header/header.component';
+import { FooterComponent } from '../../includes/footer/footer.component';
+import { BannerComponent } from '../../Components/banner/banner.component';
 
 @Component({
   selector: 'app-coporate',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,FooterComponent,BannerComponent],
   templateUrl: './coporate.component.html',
   styleUrl: './coporate.component.scss'
 })
